0e8437d7839c193d14cfc49ce02e536f3cabeee1
stesan <github@hmpf.club>
2022-11-26 15:49:20 +0900

    fix

--- backend_requests.py
+++ backend_requests.py
@@ -279,4 +279,5 @@ def searchban(screen_name):
     return returnjson
 
 
-app.run(debug=True, port=os.environ.get("PORT", 5000), host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(debug=True, port=os.environ.get("PORT", 5000), host="0.0.0.0")
